b673dd8920c884a46c809c619b0d6e81036c8ca3
Ben Frey <spartanhooah@gmail.com>
2022-12-05 19:59:02 -0500

    finish day 5

--- Day5.py
+++ Day5.py
@@ -1,37 +1,75 @@
 import collections
 
-stacks = []
 
+def build_stacks(stacks):
+    crates = line[1::4]
+    for index, crate in enumerate(crates):
+        if len(stacks) < len(crates):
+            stacks.append(collections.deque())
+
+        # append to "right"/"bottom" of deque
+        if crate != " ":
+            stacks[index].append(crate)
+
+
+def parse_instructions(line):
+    instructions = line.split()
+    count = int(instructions[1])
+    source = int(instructions[3]) - 1
+    target = int(instructions[5]) - 1
+
+    return count, source, target
+
+
+def show_answer(stacks):
+    answer = ""
+    for stack in stacks:
+        answer += stack.popleft()
+    print(answer)
+
+
+# part 1
 with open("input/day-5.txt", "r") as file:
+    stacks = []
+
     for line in file:
         if len(line.strip()) == 0:
             continue
 
         if "[" in line:
-            crates = line[1::4]
-
-            for index, crate in enumerate(crates):
-                if len(stacks) < len(crates):
-                    stacks.append(collections.deque())
-
-                # append to "right"/"bottom" of deque
-                if crate != " ":
-                    stacks[index].append(crate)
+            build_stacks(stacks)
 
         if "move" in line:
-            instructions = line.split()
-            count = int(instructions[1])
-            source = int(instructions[3]) - 1
-            target = int(instructions[5]) - 1
+            (count, source, target) = parse_instructions(line)
 
             # pop things from the "left"/"top" of the deque
             for i in range(count):
                 crate = stacks[source].popleft()
                 stacks[target].appendleft(crate)
 
-answer = ""
+show_answer(stacks)
 
-for stack in stacks:
-    answer += stack.popleft()
+# part 2
+with open("input/day-5.txt", "r") as file:
+    stacks = []
 
-print(answer)
+    for line in file:
+        if len(line.strip()) == 0:
+            continue
+
+        if "[" in line:
+            build_stacks(stacks)
+
+        if "move" in line:
+            (count, source, target) = parse_instructions(line)
+
+            moving = collections.deque()
+
+            for i in range(count):
+                moving.append(stacks[source].popleft())
+
+            while len(moving) > 0:
+                stacks[target].appendleft(moving.pop())
+
+
+show_answer(stacks)
